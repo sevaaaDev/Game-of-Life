91ff53766f8aa842b23f1d82beef3507b54cbe3c
sevaaaDev <136947471+sevaaaDev@users.noreply.github.com>
2024-11-19 16:57:27 +0700

    init

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  // grid state
+  // [current gen, prev gen]
+  // grid current gen = grid[0]
+  return <></>;
+}
+
+// APP
+// - grid state
+// - button next gen onclick
+// - button prev gen onclick
+// - button reset onclick
+// - button randomize onclick
+//
+// getNextGen(prev gen)
+//
+//
+
+export default App;
