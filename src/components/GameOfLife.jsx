527101fa1bfd48e3ae47290c63d93ee930cce952
sevaaaDev <136947471+sevaaaDev@users.noreply.github.com>
2024-11-21 16:36:01 +0700

    fix: props and style

--- src/components/GameOfLife.jsx
+++ src/components/GameOfLife.jsx
@@ -49,8 +49,6 @@ export function GameOfLife() {
 
   function setRandom() {
     let newGrid = fillRandom(currentGen);
-    // FIX: overflowing
-    console.log(newGrid);
     setGrid([newGrid, currentGen]);
   }
 
@@ -114,8 +112,8 @@ function IconPause() {
       xmlns="http://www.w3.org/2000/svg"
     >
       <path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
+        fillRule="evenodd"
+        clipRule="evenodd"
         d="M6.04995 2.74998C6.04995 2.44623 5.80371 2.19998 5.49995 2.19998C5.19619 2.19998 4.94995 2.44623 4.94995 2.74998V12.25C4.94995 12.5537 5.19619 12.8 5.49995 12.8C5.80371 12.8 6.04995 12.5537 6.04995 12.25V2.74998ZM10.05 2.74998C10.05 2.44623 9.80371 2.19998 9.49995 2.19998C9.19619 2.19998 8.94995 2.44623 8.94995 2.74998V12.25C8.94995 12.5537 9.19619 12.8 9.49995 12.8C9.80371 12.8 10.05 12.5537 10.05 12.25V2.74998Z"
         fill="currentColor"
       />
@@ -204,5 +202,5 @@ const Grid = styled.div`
   grid-template-columns: repeat(${(props) => props.$cols}, 1fr);
   aspect-ratio: 1;
   width: 90%;
-  max-width: 900px;
+  max-width: 850px;
 `;
